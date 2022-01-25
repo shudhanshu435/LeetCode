33ec597ac0c53e5bdb3e5fbb599d09b5e0e0adf1
Shudhanshu Kumar <84216064+shudhanshu435@users.noreply.github.com>
2022-01-25 17:47:47 +0530

    Added solution - LeetHub

---
+++ The XOR Gate - GFG/the-xor-gate.cpp
@@ -0,0 +1,38 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+public:
+    int xorGate(vector<int> arr,int N){
+        //code here
+        int res=arr[0];
+        for(int i=1;i<N;i++){
+            
+            res^=arr[i];
+        }return res;
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        vector<int> v;
+        for(int i = 0;i<N;i++)
+        {
+            int x;
+            cin>>x;
+            v.push_back(x);
+        }    
+        Solution ob;
+        cout << ob.xorGate(v,N) << endl;
+    }
+    return 0; 
+}  // } Driver Code Ends
