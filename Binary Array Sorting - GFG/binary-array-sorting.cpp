da7880f2d3c8600044b91972dfcc4eb1cc2bafc5
Shudhanshu Kumar <84216064+shudhanshu435@users.noreply.github.com>
2022-02-01 14:41:45 +0530

    Added solution - LeetHub

--- Binary Array Sorting - GFG/binary-array-sorting.cpp
+++ Binary Array Sorting - GFG/binary-array-sorting.cpp
@@ -1,52 +1,50 @@
 // { Driver Code Starts
-// A Sample C++ program for beginners with Competitive Programming
+//Initial template for C++
 
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 using namespace std;
 
  // } Driver Code Ends
+//User function template for C++
+
+// binArray is an array that consists only 0s and 1s
+// return sorted binary array 
 class Solution{
-  public:
-    
-    // A[]: input array
-    // N: input array
-    //Function to sort the binary array.
-    void binSort(int A[], int N)
+    public:
+    vector<int> SortBinaryArray(vector<int> binArray)
     {
-       //Your code here
-       sort(A,A+N);
-       
-       /**************
-        * No need to print the array
-        * ************/
+        // Your code goes here 
+        vector<int>vec;
+        int cnt=0;
+        int len=binArray.size();
+        for(int i=0;i<len;i++)if(binArray[i]==0)cnt++;
+        for(int i=0;i<len;i++){
+            if(i<cnt)vec.push_back(0);
+            else vec.push_back(1);
+        }return vec;
     }
 };
 
 // { Driver Code Starts.
 int main() {
-	int T;
-	cin>>T;
-	// Input the number of testcases
-	while(T--)
+	int t;
+	cin>>t;
+	
+	while(t--)
 	{
-	    int N;
-	    cin>>N; //Input size of array N
-	    int A[N]; 
+	    int n;
+	    cin>>n;
+	    vector<int> binArray(n);
 	    
-	    for(int i = 0; i  < N; i++)
-	      cin>>A[i];
-	      
-	    Solution obj;
-	    obj.binSort(A,N);
-	    
-	    for(int x:A)
-	    cout<<x<<" ";
+	    for(int i = 0; i  < n; i++)
+	      cin>>binArray[i];
+	    Solution ob;  
+	  	vector<int> result = ob.SortBinaryArray(binArray);
+	  	for(int i=0; i<n; i++)
+	  	    cout<<result[i]<<" ";
 	      
 	    cout<<endl;
 	}
 	return 0;
 }
-
-
-
   // } Driver Code Ends
