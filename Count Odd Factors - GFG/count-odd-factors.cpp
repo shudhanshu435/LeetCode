0a7c08364d8c925bbb7fe5cf448ec37479024fcb
Shudhanshu Kumar <84216064+shudhanshu435@users.noreply.github.com>
2022-02-05 01:54:08 +0530

    Added solution - LeetHub

---
+++ Count Odd Factors - GFG/count-odd-factors.cpp
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+public:
+    int count(int N){
+        // code here
+         if(N==1)return 1;
+
+        int count=0;
+        int i=1;
+        while(i<=N){
+            int r=sqrt(i);
+            if(r*r==i)count++;
+            i++;
+        }
+        return count;
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.count(N) << endl;
+    }
+    return 0; 
+}  // } Driver Code Ends
