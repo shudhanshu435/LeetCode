e893ef52a5aa88441a79e5d831fdc66d1bb24587
Shudhanshu Kumar <84216064+shudhanshu435@users.noreply.github.com>
2022-02-20 00:52:51 +0530

    Added solution - LeetHub

---
+++ Maximum Possible Sum of Products - GFG/maximum-possible-sum-of-products.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+long long MaxSum(long long int A[], long long int B[], long long int N);
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long int n;
+        cin >> n;
+        long long int a[n], b[n];
+        for(int i = 0; i < n; i++)
+            cin >> a[i];
+        for(int i = 0; i < n; i++)
+            cin >> b[i];
+        cout << MaxSum(a, b, n) << endl;
+    }
+return 0;
+}
+
+// } Driver Code Ends
+
+
+long long MaxSum(long long int A[], long long int B[], long long int N){
+    //complete the function here
+    sort(A,A+N);
+    sort(B,B+N);
+    long long sum=0;
+    for(int i=0;i<N;i++){
+        sum+=A[i]*B[i];
+    }return sum;
+}
