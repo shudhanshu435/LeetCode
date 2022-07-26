d52eb6cbbc5891ee132ada775bb3e2bc4ffa8e08
Shudhanshu Kumar <84216064+shudhanshu435@users.noreply.github.com>
2022-07-27 00:31:21 +0530

    Time: 10 ms (76.46%), Space: 10.1 MB (12.56%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(auto i: mp){
+            if(i.second ==1) ans = i.first;
+        }
+        return ans;
+    }
+};
