6e1ce335a25aecde33eea5b613b3343416c47455
Shudhanshu Kumar <84216064+shudhanshu435@users.noreply.github.com>
2023-08-22 02:04:03 +0530

    Added solution - LeetHub

---
+++ Sorted subsequence of size 3 - GFG/sorted-subsequence-of-size-3.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isSubSequence(vector<int> &v1, vector<int> &v2, int n, int m) {
+
+    if (m == 0) return true;
+    if (n == 0) return false;
+
+    if (v1[n - 1] == v2[m - 1]) return isSubSequence(v1, v2, n - 1, m - 1);
+
+    return isSubSequence(v1, v2, n - 1, m);
+}
+
+
+// } Driver Code Ends
+/*The function returns a vector containing the
+increasing sub-sequence of length 3 if present
+else returns an empty vector */
+class Solution{
+  public:
+    vector<int> find3Numbers(vector<int> arr, int N) {
+        // Your code here
+        stack<int>s;
+        int mini=INT_MAX,maxi=0;
+        for(int i=N-1;i>=2;i--){
+            maxi=max(maxi,arr[i]);
+            s.push(maxi);
+        }
+
+        for(int i=0;i<N-2;i++){
+            mini=min(mini,arr[i]);
+            if(mini<arr[i+1] and arr[i+1]<s.top())return {mini,arr[i+1],s.top()};
+            s.pop();
+        }
+        return {};
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver program to test above function
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution obj;
+        auto res = obj.find3Numbers(a, n);
+
+        // wrong format output
+        if (!res.empty() and res.size() != 3) {
+            cout << -1 << "\n";
+        }
+
+        if (res.empty()) {
+            cout << 0 << "\n";
+        } else if ((res[0] < res[1] and res[1] < res[2]) and
+                   isSubSequence(a, res, n, res.size())) {
+            cout << 1 << "\n";
+        } else {
+            cout << -1 << "\n";
+        }
+    }
+
+    return 0;
+}
+// } Driver Code Ends
