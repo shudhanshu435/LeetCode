00d3b165986f55305870d546de9302010fb507e2
Shudhanshu Kumar <84216064+shudhanshu435@users.noreply.github.com>
2023-10-09 02:54:58 +0530

    Added solution - LeetHub

--- Next Permutation - GFG/next-permutation.cpp
+++ Next Permutation - GFG/next-permutation.cpp
@@ -16,8 +16,29 @@ public:
         // next_permutation(arr.begin(),arr.end());
         // return arr;
         
+        // using implementation
+        int ind=-1;
+        for(int i=N-2;i>=0;i--){
+            if(arr[i]<arr[i+1]){
+                ind=i;
+                break;
+            }
+        }
+        if(ind==-1){
+            reverse(arr.begin(),arr.end());
+            return arr;
+        }
+        for(int i=N-1;i>=0;i--){
+            if(arr[i]>arr[ind]){
+                swap(arr[i],arr[ind]);
+                break;
+            }
+        }
         
+        reverse(arr.begin()+ind+1,arr.end());
+        return arr;
     }
+    
 };
 
 //{ Driver Code Starts.
