d7b89004e78c6e0467f12e82f3b7d4aaed5a6e1b
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2024-11-23 21:12:00 +0530

    Time: 242 ms (13.33%), Space: 56.5 MB (25.37%) - LeetHub

---
+++ 1972-rotating-the-box/1972-rotating-the-box.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        int m=box.size();
+        int n=box[0].size();
+
+        vector<vector<char>>vec(n, vector<char>(m));
+        //transpose
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                vec[i][j]=box[j][i];
+            }
+        }
+
+        //90 degree rotate
+        for(vector<char>& row: vec){
+            reverse(row.begin(),row.end());
+        }
+
+        for(int j=0;j<m;j++){
+            for(int i=n-1;i>=0;i--){
+                if(vec[i][j]=='.'){
+                    int row=-1;
+                    for(int k=i-1;k>=0;k--){
+                        if(vec[k][j]=='*'){
+                            break;
+                        } else if(vec[k][j]=='#'){
+                            row=k;
+                            break;
+                        }
+                    }
+                    if(row!=-1){
+                        vec[i][j]='#';
+                        vec[row][j]='.';
+                    }
+                }
+            }
+        }
+        return vec;
+    }
+};
