12b8cb72f1070c16a02248bfb4e3f5ade320d3d4
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2024-11-23 21:31:46 +0530

    Time: 187 ms (75.83%), Space: 56.2 MB (96.34%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.cpp
+++ 1972-rotating-the-box/1972-rotating-the-box.cpp
@@ -18,24 +18,39 @@ public:
         }
 
         for(int j=0;j<m;j++){
+            int space=n-1;
             for(int i=n-1;i>=0;i--){
-                if(vec[i][j]=='.'){
-                    int row=-1;
-                    for(int k=i-1;k>=0;k--){
-                        if(vec[k][j]=='*'){
-                            break;
-                        } else if(vec[k][j]=='#'){
-                            row=k;
-                            break;
-                        }
-                    }
-                    if(row!=-1){
-                        vec[i][j]='#';
-                        vec[row][j]='.';
-                    }
+                if(vec[i][j]=='*'){
+                    space=i-1;
+                    continue;
+                }
+                if(vec[i][j]=='#'){
+                    vec[i][j]='.';
+                    vec[space][j]='#';
+                    space--;
                 }
             }
         }
+
+        // for(int j=0;j<m;j++){
+        //     for(int i=n-1;i>=0;i--){
+        //         if(vec[i][j]=='.'){
+        //             int row=-1;
+        //             for(int k=i-1;k>=0;k--){
+        //                 if(vec[k][j]=='*'){
+        //                     break;
+        //                 } else if(vec[k][j]=='#'){
+        //                     row=k;
+        //                     break;
+        //                 }
+        //             }
+        //             if(row!=-1){
+        //                 vec[i][j]='#';
+        //                 vec[row][j]='.';
+        //             }
+        //         }
+        //     }
+        // }
         return vec;
     }
 };
