788fc6b1145c09a3fc9c3b9d9401a7b5a78a871a
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2024-12-11 18:06:52 +0530

    Time: 3 ms (96.8%), Space: 10.8 MB (76.12%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -1,27 +1,52 @@
 class Solution {
-public://aabbccccde
+public:
     int maximumLength(string s) {
+        // int n=s.length();
+        // unordered_map<string,int>mp;
+        
+        // for(int i=0;i<n;i++){
+        //     string curr="";
+        //     for(int j=i;j<n;j++){
+        //         if(curr.empty() or curr.back()==s[j]){
+        //             curr+=s[j];
+        //         }
+        //         else {
+        //             break;
+        //         }
+        //         mp[curr]++;
+        //     }
+        // }
+        // int ans=-1;
+        // for(auto i:mp)cout<<i.first<<" "<<i.second<<endl;
+        // for(auto i:mp){
+        //     string a=i.first;
+        //     int freq=i.second;
+        //     int len=a.length();
+        //     if(freq>=3)ans=max(ans,len);
+        // }
+        // return ans;
+
+//   aaaabb
+
         int n=s.length();
-        unordered_map<string,int>mp;
+        map<pair<int,int>,int>mp;
+        
         for(int i=0;i<n;i++){
-            string curr="";
+            int cnt=0;
             for(int j=i;j<n;j++){
-                if(curr.empty() or curr.back()==s[j]){
-                    curr+=s[j];
+                if(s[i]==s[j]){
+                    cnt++;
                 }
                 else {
                     break;
                 }
-                mp[curr]++;
+                mp[make_pair(s[i],cnt)]++; 
             }
         }
         int ans=-1;
-        for(auto i:mp)cout<<i.first<<" "<<i.second<<endl;
         for(auto i:mp){
-            string a=i.first;
             int freq=i.second;
-            int len=a.length();
-            if(freq>=3)ans=max(ans,len);
+            if(freq>=3)ans=max(ans,i.first.second);
         }
         return ans;
     }
