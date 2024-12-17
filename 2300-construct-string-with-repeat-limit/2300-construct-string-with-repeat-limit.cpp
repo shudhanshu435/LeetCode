fc17df87efbf6a1e820728fde05f646c623faabc
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2024-12-18 00:36:42 +0530

    Time: 23 ms (79.75%), Space: 28.3 MB (21.08%) - LeetHub

--- 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.cpp
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.cpp
@@ -1,69 +1,76 @@
 class Solution {
 public:
     string repeatLimitedString(string s, int rl) {
-        // map<char,int,greater<int>>mp;
-        // for(char i:s){
-        //     mp[i]++;
-        // }
-        // string ans="";
-        // priority_queue<pair<char, int>> pq;
-        
-        // for (auto pair : mp) {
-        //     pq.push({pair.first, pair.second});
-        // }
-
-        // while(1){
-        //     char c=pq.top().first;
-        //     int cc=pq.top().second;
-        //     if(cc<=rl){
-        //         while(cc--){
-        //             ans+=c;
-        //         }
-        //     }
-        //     else{
-        //         cc=cc-rl;
-        //         while(rl--){
-        //             ans+=c;
-        //         }
-
-        //     }
-        // }
-
-        
-        // return "";
         int n=s.length();
-        vector<int>count(26,0);
-        for(int i=0;i<n;i++){
+        vector<int>count(26,0); 
+        for(int i=0;i<n;i++){ 
             count[s[i]-'a']++;
         }
-
-        string result="";
-
-        int i=25;
-        while(i>=0){
-            if(count[i]==0){
-                i--;
-                continue;
-            }
-
-            char ch= 'a'+i;
-            int freq=min(rl,count[i]);
-            result.append(freq,ch);
-
-            count[i]-=freq;
-
+        string res="";
+        priority_queue<char> pq;
+        
+        for (int i=0;i<26;i++) {
             if(count[i]>0){
-                int j=i-1;
-
-                while(j>=0 and count[j]==0)j--;
-                if(j<0)break;
-
-                result.push_back('a'+j);
-                count[j]--;
+                char ch='a'+i;
+                pq.push(ch);
             }
-
         }
-        return result;
+
+        while(!pq.empty()){
+            char c=pq.top();
+            pq.pop();
+            int freq=min(count[c-'a'],rl);
+            res.append(freq,c);
+
+            count[c-'a']-=freq;
+
+            if(count[c-'a']>0 and !pq.empty()){
+                char nextc=pq.top();
+                pq.pop();
+                res.push_back(nextc);
+                count[nextc-'a']--;
+
+                if(count[nextc-'a']>0)pq.push(nextc);
+
+                pq.push(c);
+            }
+        }
+
+        
+        return res;
+        // int n=s.length();
+        // vector<int>count(26,0); //sc O(26)
+        // for(int i=0;i<n;i++){ // O(n)
+        //     count[s[i]-'a']++;
+        // }
+
+        // string result="";
+
+        // int i=25; //O(n)
+        // while(i>=0){
+        //     if(count[i]==0){
+        //         i--;
+        //         continue;
+        //     }
+
+        //     char ch= 'a'+i;
+        //     int freq=min(rl,count[i]);
+        //     result.append(freq,ch);
+
+        //     count[i]-=freq;
+
+        //     if(count[i]>0){
+        //         int j=i-1;
+
+        //         while(j>=0 and count[j]==0)j--;//O(26)
+        //         if(j<0)break;
+
+        //         result.push_back('a'+j);
+        //         count[j]--;
+        //     }
+
+        // }
+        // return result;
     }
 };
 
