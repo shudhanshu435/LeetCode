e19f7c56a8a6c60d8f8aa289d195c5c721755760
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-01-12 23:48:32 +0530

    Time: 11 ms (76.9%), Space: 30.1 MB (70.56%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -4,24 +4,46 @@ public:
         int n=s.length();
 
         if(n&1)return 0;
-        stack<int>op;
-        stack<int>opcl;
+        // stack<int>op;
+        // stack<int>opcl;
+        // for(int i=0;i<n;i++){
+        //     if(locked[i]=='0')opcl.push(i);
+        //     else if(s[i]=='(')op.push(i);
+        //     else if(s[i]==')'){
+        //         if(!op.empty())op.pop();
+        //         else if(!opcl.empty())opcl.pop();
+        //         else return 0;
+        //     }
+        // }
+
+        // while(!op.empty() and !opcl.empty() and op.top()<opcl.top()){
+        //     op.pop();
+        //     opcl.pop();
+        // }
+
+
+        //2nd approach
+        int op=0;
         for(int i=0;i<n;i++){
-            if(locked[i]=='0')opcl.push(i);
-            else if(s[i]=='(')op.push(i);
-            else if(s[i]==')'){
-                if(!op.empty())op.pop();
-                else if(!opcl.empty())opcl.pop();
-                else return 0;
+            if(s[i]=='(' or locked[i]=='0'){
+                op++;
             }
+            else op--;
+
+            if(op<0)return 0;
         }
 
-        while(!op.empty() and !opcl.empty() and op.top()<opcl.top()){
-            op.pop();
-            opcl.pop();
+        int cl=0;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]==')' or locked[i]=='0'){
+                cl++;
+            }
+            else cl--;
+
+            if(cl<0)return 0;
         }
 
-        return op.empty();
+        return 1;
     }
 };
 
