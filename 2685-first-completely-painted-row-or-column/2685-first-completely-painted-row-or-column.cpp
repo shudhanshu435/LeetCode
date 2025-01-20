1dadff2c020baec725dddfa5f33381dcd8b662bc
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-01-20 23:35:37 +0530

    Time: 109 ms (37.8%), Space: 161.5 MB (71.34%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -1,44 +1,30 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        int m=mat.size();
-        int n=mat[0].size();
-        cout<<m<<" "<<n<<endl;
-        unordered_map<int,pair<int,int>>mm;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                mm[mat[i][j]]={i,j};
+        int n=mat.size();
+        int m=mat[0].size();
+        
+        unordered_map<int,int>mp;
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]]=i;
+        }
+        int ans=INT_MAX;
+        for(int i=0;i<n;i++){
+            int maxi=-1;
+            for(int j=0;j<m;j++){
+                maxi=max(maxi,mp[mat[i][j]]);
             }
+            ans=min(ans,maxi);
         }
 
-        // for(auto v:mm){
-        //     cout<<v.first<<" - ["<<v.second.first<<" , "<<v.second.second<<"]"<<endl;
-        // }
-        
-        map<vector<int>,int>mp;
-        int x,y;
-        int ans=0;
-        for(int i=0;i<m*n;i++){
-            vector<int>v,t;
-            v.push_back(-1);
-            t.push_back(-2);
-            x=mm[arr[i]].first;
-            y=mm[arr[i]].second;
-            cout<<x<<" "<<y<<endl;
-            v.push_back(x);
-            t.push_back(y);
-            mp[v]++;
-            mp[t]++;
-            cout<<mp[v]<<" @@@ "<<mp[t]<<endl;
-            if(mp[v]==n or mp[t]==m){
-                ans=i;
-                break;
+        for(int i=0;i<m;i++){
+            int maxi=-1;
+            for(int j=0;j<n;j++){
+                maxi=max(maxi,mp[mat[j][i]]);
             }
+            ans=min(ans,maxi);
         }
-        // for(auto m:mp){
-        //     for(auto v:m.first)cout<<v<<" ";
-        //     cout<<m.second<<endl;
-        // }
+
         return ans;
     }
 };
