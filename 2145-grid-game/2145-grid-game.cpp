4b9c6eabd09175382813f0426d9f7164fe806a3c
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-01-21 22:16:36 +0530

    Time: 0 ms (100%), Space: 66.7 MB (76.92%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
+        long long p = accumulate(grid[0].begin(),grid[0].end(),0LL);
+        long long ans=LLONG_MAX;
         int n=grid[0].size();
-        vector<long long>p(n);
-        vector<long long>s(n);
-        p[0]=grid[0][0];
-        for(int i=1;i<n;i++){
-            p[i]=grid[0][i]+p[i-1];
-        }
-        s[n-1]=grid[1][n-1];
-        for(int i=n-2;i>=0;i--){
-            s[i]=grid[1][i]+s[i+1];
-        }
-        long long ans=5*1ll*INT_MAX;
+        long long s=0;
         for(int i=0;i<n;i++){
-            long long maxii=max(s[0]-s[i],p[n-1]-p[i]);
-            ans=min(ans,maxii);
+            p-=grid[0][i];
+            ans=min(ans,max(p,s));
+            s+=grid[1][i];
         }
         return ans;
     }
