bb4daaa2b86f16c956440a681894fbb75b366c27
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-01-22 16:50:43 +0530

    Time: 234 ms (15.9%), Space: 103.2 MB (95.51%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -5,14 +5,15 @@ public:
         int m=iss.size();
         int n=iss[0].size();
         priority_queue<pp,vector<pp>,greater<pp>>pq;
-        vector<vector<int>>vec(m, vector<int>(n,0));
-        vector<vector<int>>vis(m, vector<int>(n,false));
+        vector<vector<int>>vec(m, vector<int>(n,INT_MAX));
+        // vector<vector<int>>vis(m, vector<int>(n,false));
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(iss[i][j]==1){
                     pq.push({0,{i,j}});
-                    vis[i][j]=true;
+                    // vis[i][j]=true;
+                    vec[i][j]=0;
                 }
             }
         }
@@ -22,23 +23,27 @@ public:
             int jj=pq.top().second.second;
             pq.pop();
             
-            if(ii-1>=0 and vis[ii-1][jj]==false){
-                vis[ii-1][jj]=true;
+            // if(ii-1>=0 and vis[ii-1][jj]==false){
+            if(ii-1>=0 and vec[ii-1][jj]==INT_MAX){
+                // vis[ii-1][jj]=true;
                 vec[ii-1][jj]=val+1;
                 pq.push({val+1,{ii-1,jj}});
             }
-            if(jj-1>=0 and vis[ii][jj-1]==false){
-                vis[ii][jj-1]=true;
+            // if(jj-1>=0 and vis[ii][jj-1]==false){
+            if(jj-1>=0 and vec[ii][jj-1]==INT_MAX){
+                // vis[ii][jj-1]=true;
                 vec[ii][jj-1]=val+1;
                 pq.push({val+1,{ii,jj-1}});
             }
-            if(ii+1<m and vis[ii+1][jj]==false){
-                vis[ii+1][jj]=true;
+            // if(ii+1<m and vis[ii+1][jj]==false){
+            if(ii+1<m and vec[ii+1][jj]==INT_MAX){
+                // vis[ii+1][jj]=true;
                 vec[ii+1][jj]=val+1;
                 pq.push({val+1,{ii+1,jj}});
             }
-            if(jj+1<n and vis[ii][jj+1]==false){
-                vis[ii][jj+1]=true;
+            // if(jj+1<n and vis[ii][jj+1]==false){
+            if(jj+1<n and vec[ii][jj+1]==INT_MAX){
+                // vis[ii][jj+1]=true;
                 vec[ii][jj+1]=val+1;
                 pq.push({val+1,{ii,jj+1}});
             }
