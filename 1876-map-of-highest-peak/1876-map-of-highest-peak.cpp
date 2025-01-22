751457b99b35242c92fc09c13574ab2f1e66ae0d
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-01-22 17:01:13 +0530

    Time: 239 ms (15.9%), Space: 103 MB (95.51%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -6,46 +6,42 @@ public:
         int n=iss[0].size();
         priority_queue<pp,vector<pp>,greater<pp>>pq;
         vector<vector<int>>vec(m, vector<int>(n,INT_MAX));
-        // vector<vector<int>>vis(m, vector<int>(n,false));
+        int cnt=m*n;
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(iss[i][j]==1){
                     pq.push({0,{i,j}});
-                    // vis[i][j]=true;
+                    cnt--;
                     vec[i][j]=0;
                 }
             }
         }
-        while(!pq.empty()){
+        while(!pq.empty() and cnt){
             int val=pq.top().first;
             int ii=pq.top().second.first;
             int jj=pq.top().second.second;
             pq.pop();
             
-            // if(ii-1>=0 and vis[ii-1][jj]==false){
             if(ii-1>=0 and vec[ii-1][jj]==INT_MAX){
-                // vis[ii-1][jj]=true;
                 vec[ii-1][jj]=val+1;
                 pq.push({val+1,{ii-1,jj}});
+                cnt--;
             }
-            // if(jj-1>=0 and vis[ii][jj-1]==false){
             if(jj-1>=0 and vec[ii][jj-1]==INT_MAX){
-                // vis[ii][jj-1]=true;
                 vec[ii][jj-1]=val+1;
                 pq.push({val+1,{ii,jj-1}});
+                cnt--;
             }
-            // if(ii+1<m and vis[ii+1][jj]==false){
             if(ii+1<m and vec[ii+1][jj]==INT_MAX){
-                // vis[ii+1][jj]=true;
                 vec[ii+1][jj]=val+1;
                 pq.push({val+1,{ii+1,jj}});
+                cnt--;
             }
-            // if(jj+1<n and vis[ii][jj+1]==false){
             if(jj+1<n and vec[ii][jj+1]==INT_MAX){
-                // vis[ii][jj+1]=true;
                 vec[ii][jj+1]=val+1;
                 pq.push({val+1,{ii,jj+1}});
+                cnt--;
             }
 
         }
