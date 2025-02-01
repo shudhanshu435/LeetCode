d93af5d15465b756d5368a23bc0cb48eff0c179d
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-02-01 07:25:24 +0530

    Time: 0 ms (100%), Space: 27.7 MB (61.71%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0;i<n-1;i++){
-            if((nums[i]&1)^(nums[i+1]&1))continue;
-            else return false;
+        if(nums.size()==1)return 1;
+        for(int i=0;i<nums.size()-1;i++){
+            if((nums[i]+nums[i+1]) %2 ==0)return 0;
         }
-        return true;
+        return 1;
     }
 };
