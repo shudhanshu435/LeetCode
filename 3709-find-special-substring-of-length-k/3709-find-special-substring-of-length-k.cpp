aa1625b14d9d1b1f9c2c657a758ff8e529edfd23
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-02-17 18:31:20 +0530

    Time: 3 ms (100%), Space: 9.5 MB (50%) - LeetHub

--- 3709-find-special-substring-of-length-k/3709-find-special-substring-of-length-k.cpp
+++ 3709-find-special-substring-of-length-k/3709-find-special-substring-of-length-k.cpp
@@ -15,18 +15,22 @@ public:
         if(mp.size()==1){
             if(s[i]!=s[i-1])return 1;
         }
-        set<char>ss;
-        int l;
-        for(l=n-k;l<n;l++)ss.insert(s[l]);
-        if(ss.size()==1 and s[l-1]!=s[n-k-1])return 1;   
+        // set<char>ss;
+        // int l;
+        // for(l=n-k;l<n;l++)ss.insert(s[l]);
+        // if(ss.size()==1 and s[l-1]!=s[n-k-1])return 1;   
 
         int kk=0;
         int le=0,rt=k+1;
-        for(;i<n-1;i++){
+        for(;i<n;i++){
             mp[s[i]]++;
             mp[s[kk]]--;
             if(mp[s[kk]]==0)mp.erase(s[kk]);
-            if(mp.size()==1 and s[le]!=s[le+1] and s[rt]!=s[rt-1])return 1;
+            bool left=true,right=true;
+            if(rt<n and s[rt]==s[rt-1])right=false;
+            if(le>=0 and s[le]==s[le+1])left=false;
+            if(mp.size()==1 and right and left)return 1;
+            // if(mp.size()==1 and s[le]!=s[le+1] and s[rt]!=s[rt-1])return 1;
             le++;rt++;
             kk++;
         }
