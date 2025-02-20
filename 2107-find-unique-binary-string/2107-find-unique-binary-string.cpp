e919945f68dc4fd87e5438169edf355a4af63ff6
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-02-20 19:22:09 +0530

    Time: 0 ms (100%), Space: 12.7 MB (69.88%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -17,37 +17,44 @@ public:
     //     solve(s,n,mp,ans);
     //     s.pop_back();
     // }
-    string dectobin(long long n, int size){
-        string ans="";
-        while(size--){
-            if(n&1)ans+='1';
-            else ans+='0';
-            n/=2;
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
-    }
-    long long bintodec(string& s){
-        int n=s.length();
-        long long ans=0;
-        for(int i=0;i<n;i++){
-            if(s[n-1-i]=='1')
-                ans+=(1<<i);
-        }
-        return ans;
-    }
+    
+    // string dectobin(long long n, int size){
+    //     string ans="";
+    //     while(size--){
+    //         if(n&1)ans+='1';
+    //         else ans+='0';
+    //         n/=2;
+    //     }
+    //     reverse(ans.begin(),ans.end());
+    //     return ans;
+    // }
+    // long long bintodec(string& s){
+    //     int n=s.length();
+    //     long long ans=0;
+    //     for(int i=0;i<n;i++){
+    //         if(s[n-1-i]=='1')
+    //             ans+=(1<<i);
+    //     }
+    //     return ans;
+    // }
     string findDifferentBinaryString(vector<string>& nums) {
-        unordered_map<long long,int>mp;
-        for(auto i:nums){
-            mp[bintodec(i)]++;
+        // unordered_map<long long,int>mp;
+        // for(auto i:nums){
+        //     mp[bintodec(i)]++;
+        // }
+        // int n=nums.size();
+        // long long i=0;
+        // while(1){
+        //     if(mp[i]==0){
+        //         return dectobin(i,n);
+        //     }
+        //     i++;
+        // }
+        int n = nums.size();
+        string ans;
+        for (int i = 0; i < n; i++) {
+            ans += (nums[i][i] == '0') ? '1' : '0';
         }
-        int n=nums.size();
-        long long i=0;
-        while(1){
-            if(mp[i]==0){
-                return dectobin(i,n);
-            }
-            i++;
-        }
+        return ans;
     }
 };
