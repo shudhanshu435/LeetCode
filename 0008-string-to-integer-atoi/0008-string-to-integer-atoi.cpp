eaa68e13fd838d175d8b1e4946231aec66d0ef99
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-02-24 19:32:06 +0530

    Time: 0 ms (100%), Space: 9.1 MB (57.72%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        if(s=="-2147483647")return -2147483647;
         int i=0;
         int n=s.length();
         while(i<n and s[i]==' ')i++;
-        char sign='+';
-        if(i<n and s[i]=='-'){
-            sign='-';
+        int sign=1;
+        if(i<n and (s[i]=='-' or s[i]=='+')){
+            if(s[i]=='-')sign=-1;
             i++;
         }
-        else if(i<n and s[i]=='+')i++;
         long long ans=0;
         while(i<n and s[i]=='0')i++;
         while(i<n and s[i]>='0' and s[i]<='9'){
             ans=(ans*10+(s[i]-'0'));
-            if(ans>INT_MAX)ans=INT_MAX;
-            // if(ans<INT_MIN)ans=INT_MIN;
+            if(sign==-1){
+                if(ans>2147483648)return -2147483648;
+            }
+            else{
+                if(ans>2147483647)return 2147483647;
+            }
             i++;
         }
-        if(ans!=0){
-            if(sign=='-'){
-                if(ans>=INT_MAX)return -1*(ans+1);
-                return -1*(ans);
-            }
-            return ans;
-        }
-        return 0;
+        return sign*ans;
     }
 };
