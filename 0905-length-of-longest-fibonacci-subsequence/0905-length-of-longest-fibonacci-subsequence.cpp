8d25a1f5ecdc11e24adae581ba3814757777ca0e
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-02-27 10:10:18 +0530

    Time: 381 ms (22.86%), Space: 13.7 MB (88.89%) - LeetHub

--- 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
@@ -3,34 +3,27 @@ public:
     int lenLongestFibSubseq(vector<int>& arr) {
         long long ans=0;
         int n=arr.size();
+        long long cnt=0;
         for(int i=0;i<n-2;i++){
-            long long cnt=1;
             for(int j=i+1;j<n-1;j++){
                 long long a=arr[i];
-                int ind=j;
-                cnt=2;
                 long long b=arr[j];
-                // int c=a+b;
-                bool nf=0;
-                while(ind<n){
+                cnt=2;
+                while(1){
                     long long newsum=a+b;
-                    // cout<<newsum<<endl;
                     auto it=lower_bound(arr.begin(),arr.end(),newsum);
                     int ind=distance(arr.begin(),it);
-                    // cout<<ind<<endl;
+
                     if(ind<n and arr[ind]==newsum){
                         a=b;
                         b=*it;
                         cnt++;
-                        // cout<<ind<<" "<<cnt<<endl;
                     }
                     else{
                         ans=max(ans,cnt);
-                        bool nf=1;
                         break;
                     }
                 }
-                // if(nf)break;
             }
         }
         if(ans<3)return 0;
