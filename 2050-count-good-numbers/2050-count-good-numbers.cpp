6cc5e783a114aa177d4d8615f84495d53e0dfc83
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-02-28 19:30:52 +0530

    Time: 0 ms (100%), Space: 8 MB (3.4%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -2,25 +2,32 @@ class Solution {
 public:
     int mod = 1e9 + 7;
 
-    long long modPow(long long b, long long exp, int mod){
-        long long ans=1;
-        while(exp>0){
-            if(exp&1){
-                ans=(ans*b)%mod;
-                exp--;
-            }
-            else{
-                b=(b*b)%mod;
-                exp/=2;
-            }
-        }
-        return ans;
+    long long modPow(long long b, long long exp, long long ans){
+        //recursive
+        if(exp==0)return ans;
+        if(exp&1)return modPow(b,exp-1,(ans*b)%mod);
+        return modPow((b*b)%mod,exp/2,ans);
+
+        ////iterative
+        // long long ans=1;
+        // while(exp>0){
+        //     if(exp&1){
+        //         ans=(ans*b)%mod;
+        //         exp--;
+        //     }
+        //     else{
+        //         b=(b*b)%mod;
+        //         exp/=2;
+        //     }
+        // }
+        // return ans;
     }
 
     int countGoodNumbers(long long n) {
         long long no_4=n/2;
         long long no_5=(n+1)/2;
+        long long ans=1;
 
-        return (modPow(5, no_5, mod) * modPow(4, no_4, mod)) % mod;
+        return (modPow(5, no_5, ans) * modPow(4, no_4, ans)) % mod;
     }
 };
