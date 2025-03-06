d0d55804f76005a7d7fcbf596a1e4e07fc718334
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-03-06 19:46:11 +0530

    Time: 7 ms (26.05%), Space: 54.5 MB (57.72%) - LeetHub

--- 2634-minimum-common-value/2634-minimum-common-value.cpp
+++ 2634-minimum-common-value/2634-minimum-common-value.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
+    bool bin(vector<int>&nums, int m){
+        int i=0,j=nums.size()-1;
+        while(i<=j){
+            int mid=(i+j)/2;
+            if(nums[mid]==m)return true;
+            else if(nums[mid]>m)j=mid-1;
+            else i=mid+1;
+        }
+        return 0;
+    }
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         int n=nums1.size();
         int m=nums2.size();
         int i=0,j=0;
-        while(i<n and j<m){
-            if(nums1[i]==nums2[j])return nums1[i];
-            else if(nums1[i]<nums2[j])i++;
-            else j++;
+        for(auto i:nums1){
+            if(bin(nums2,i))return i;
         }
         return -1;
     }
