1ab348f00d96e4c132fd257397a8beef32f4af4e
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-03-06 10:06:29 +0530

    Time: 0 ms (100%), Space: 21.2 MB (95.85%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
     int binNeg(vector<int>& nums, int l, int r){
-        int ans = 0;
         while(l<=r){
             int mid=(l+r)/2;
-            if(nums[mid]>=0){
-                r=mid-1;
-            }else{
-                ans = mid+1;
-                l=mid+1;
+            if(nums[mid]<0){
+                if(nums[mid+1]>=0)return mid+1;
+                else l=mid+2;
             }
+            else r=mid-1;
         }
-        return ans;
+        return l;
     }
     int binPos(vector<int>& nums, int l, int r){
-        int ans = 0, n=r+1;
         while(l<=r){
             int mid=(l+r)/2;
-            if(nums[mid]<=0){
-                l=mid+1;
+            if(nums[mid]>0){
+                if(nums[mid-1]<=0)return mid;
+                else r=mid-2;
             }
-            else {
-                ans=n-mid;
-                r=mid-1;
-            }
+            else l=mid+1;
         }
-        return ans;
+        return r+1;
     }
     int maximumCount(vector<int>& nums) {
         int n=nums.size();
         if(nums[0]>0 or nums[n-1]<0)return n;
         int neg=binNeg(nums,0,n-1);
         int pos=binPos(nums,0,n-1);
-        // cout<<pos<<" "<<neg<<endl;
-        return max(neg,pos);
+        cout<<neg<<" "<<pos<<"\n";
+        return max(neg,n-pos);
     }
 };
