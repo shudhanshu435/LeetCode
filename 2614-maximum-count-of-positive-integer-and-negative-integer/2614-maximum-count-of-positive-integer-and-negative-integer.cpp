1a5171938a66e1cf8d74fc29c0f195872598f69c
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-03-06 10:16:17 +0530

    Time: 0 ms (100%), Space: 21.5 MB (44.29%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -5,22 +5,22 @@ public:
             int mid=(l+r)/2;
             if(nums[mid]<0){
                 if(nums[mid+1]>=0)return mid+1;
-                else l=mid+2;
+                else l=mid+1;
             }
             else r=mid-1;
         }
-        return l;
+        return 0;
     }
     int binPos(vector<int>& nums, int l, int r){
         while(l<=r){
             int mid=(l+r)/2;
             if(nums[mid]>0){
                 if(nums[mid-1]<=0)return mid;
-                else r=mid-2;
+                else r=mid-1;
             }
             else l=mid+1;
         }
-        return r+1;
+        return nums.size();
     }
     int maximumCount(vector<int>& nums) {
         int n=nums.size();
