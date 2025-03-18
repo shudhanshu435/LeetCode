65778cc9b7e0bd7ab6eda646aae5fa8c9826cd70
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-03-18 11:30:15 +0530

    Time: 0 ms (100%), Space: 60.8 MB (99.56%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -2,29 +2,19 @@ class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
         // unordered_map<int,int>mp;
-        vector<int>vec(32,0);
-        int ans=1;
-        int k=0;
+        long long sum=0,xorr=0;
+        int k=0,ans=0;
         for(int i=0;i<nums.size();i++){
-            bool nice=1;
-            for(int j=0;j<32;j++){
-                if((nums[i]>>j)&1)vec[j]++;
-                if(vec[j]==2){
-                    nice=0;
-                }
+            sum+=nums[i];
+            xorr^=nums[i];
+            if(xorr==sum){
+                ans=max(ans,i-k+1);
             }
-            if(nice)ans=max(ans,i-k+1);
             else{
-                while(1){
-                    int cnt=0;
-                    for(int j=0;j<32;j++){
-                        if((nums[k]>>j)&1)vec[j]--;
-                        if(vec[j]<=1){
-                            cnt++;
-                        }
-                    }
+                while(xorr!=sum){
+                    sum-=nums[k];
+                    xorr^=nums[k];
                     k++;
-                    if(cnt==32)break;
                 }
             }
         }
