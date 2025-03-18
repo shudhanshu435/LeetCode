4426a68d17cb13011910a4db753265267fe3fab6
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-03-18 10:19:34 +0530

    Time: 583 ms (5.04%), Space: 61.1 MB (28.44%) - LeetHub

---
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int ans=1;
+        int k=0;
+        for(int i=0;i<nums.size();i++){
+            bool nice=1;
+            for(int j=0;j<32;j++){
+                if((nums[i]>>j)&1)mp[j]++;
+                if(mp[j]==2){
+                    nice=0;
+                }
+            }
+            if(nice)ans=max(ans,i-k+1);
+            else{
+                while(1){
+                    int cnt=0;
+                    for(int j=0;j<32;j++){
+                        if((nums[k]>>j)&1)mp[j]--;
+                        if(mp[j]<=1){
+                            cnt++;
+                        }
+                    }
+                    k++;
+                    if(cnt==32)break;
+                }
+            }
+        }
+        return ans;
+    }
+};
