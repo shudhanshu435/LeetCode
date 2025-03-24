3a51462dd7b1986fed94eb013bfe4a31c761d44e
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-03-24 09:13:34 +0530

    Time: 72 ms (48.98%), Space: 126.5 MB (80.1%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -8,7 +8,7 @@ public: // [1,3],[5,7],[9,10]
             if(meet[i][0]>meet[i-1][1]){
                 ans+=meet[i][0]-meet[i-1][1]-1;
             }
-            meet[i][1]=max(meet[i][1],meet[i-1][1]);
+            else meet[i][1]=max(meet[i][1],meet[i-1][1]);
         }
         ans+=meet[0][0]-1+days-meet[n-1][1];
         return ans;
