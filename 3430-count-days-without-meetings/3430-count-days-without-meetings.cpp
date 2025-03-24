58eb5fa298099b2162a1914307c29760fac21fbb
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-03-24 09:09:20 +0530

    Time: 71 ms (53.06%), Space: 126.6 MB (43.2%) - LeetHub

---
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public: // [1,3],[5,7],[9,10]
+    int countDays(int days, vector<vector<int>>& meet) {
+        int n=meet.size();
+        sort(meet.begin(),meet.end());
+        int ans=0;
+        for(int i=1;i<n;i++){
+            if(meet[i][0]>meet[i-1][1]){
+                ans+=meet[i][0]-meet[i-1][1]-1;
+            }
+            meet[i][1]=max(meet[i][1],meet[i-1][1]);
+        }
+        ans+=meet[0][0]-1+days-meet[n-1][1];
+        return ans;
+    }
+};
