8adde06f51397271fd3a903032f94e18e2df7d99
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-04-01 18:49:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    long long mostPoints(vector<vector<int>>& questions) {
-        int n=questions.size();
-        vector<long long>vec(n);
-        vec[n-1]=questions[n-1][0];
+    long long mostPoints(vector<vector<int>>& q) {
+        int n=q.size();
         for(int i=n-2;i>=0;i--){
-            if(i+questions[i][1]+1<n)vec[i]=max((long long)questions[i][0]+vec[i+questions[i][1]+1],vec[i+1]);
-            else vec[i]=max(vec[i+1],(long long)questions[i][0]);
+            if(i+q[i][1]+1<n)q[i][0]=max(q[i][0]+q[i+q[i][1]+1][0],q[i+1][0]);
+            else q[i][0]=max(q[i+1][0],q[i][0]);
         }
-        for(auto i:vec)cout<<i<<" ";
-        cout<<endl;
-        return vec[0];
+        // for(auto i:vec)cout<<i<<" ";
+        // cout<<endl;
+        return q[0][0];
     }
 };
