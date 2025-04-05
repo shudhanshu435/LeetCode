08cfb6f326a93f66e44d6744f8d3b114ccd12c01
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-04-05 14:05:33 +0530

    Time: 123 ms (5.13%), Space: 54.1 MB (5%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
+    void gensubsets(vector<int>&nums, int index, vector<int>subset, vector<vector<int>>&sset){
+        if(index==nums.size()){
+            sset.push_back(subset);
+            return;
+        }
+
+        subset.push_back(nums[index]);
+        gensubsets(nums,index+1,subset,sset);
+        subset.pop_back();
+        gensubsets(nums,index+1,subset,sset);
+    }
     int subsetXORSum(vector<int>& nums) {
-        int n=nums.size();
-        int ans=nums[0];
-        for(int i=1;i<n;i++)ans=ans|nums[i];
-        ans<<=(n-1);
-        return ans;
+        vector<vector<int>>sset;
+        gensubsets(nums,0,{},sset);
+        int sum=0;
+        for(auto i:sset){
+            int x=0;
+            for(auto j:i){
+                x^=j;
+            }
+            sum+=x;
+        }
+        return sum;
     }
 };
