7afc98dc1cd8f8a626b1921c0e7d409f6cebb73a
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-04-07 14:00:54 +0530

    Time: 310 ms (5.88%), Space: 242.8 MB (5.25%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public: 
-    bool pos(vector<int>&nums, int ind, int sum, int curr){
+    bool pos(vector<int>&nums, int ind, int sum, int curr, vector<vector<int>>& dp){
         if(curr==sum)return 1;
-        if(ind==nums.size())return 1;
-        curr+=nums[ind];
-        return pos(nums,ind+1,sum,curr);
-        curr-=nums[ind];
-        return pos(nums,ind+1,sum,curr);
+        if(ind==nums.size())return 0;
+        if(dp[ind][curr]!=-1)return dp[ind][curr];
+        bool take= pos(nums,ind+1,sum,curr+nums[ind],dp);
+        bool nottake= pos(nums,ind+1,sum,curr,dp);
+
+        return dp[ind][curr]=take or nottake;
     }
     bool canPartition(vector<int>& nums) {
         int sum=accumulate(nums.begin(),nums.end(),0);
+        vector<vector<int>> dp(nums.size(), vector<int>(sum + 1, -1));
         if(sum&1)return 0;
         sum/=2;
-        return pos(nums,0,sum,0);
+        return pos(nums,0,sum,0,dp);
     }
 };
