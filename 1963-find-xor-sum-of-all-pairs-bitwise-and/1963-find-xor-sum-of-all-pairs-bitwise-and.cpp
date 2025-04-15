22b53ab9d2adcaa536e9bcacc3149d8797c1124a
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-04-15 10:59:15 +0530

    Time: 39 ms (7.1%), Space: 97.3 MB (77.51%) - LeetHub

--- 1963-find-xor-sum-of-all-pairs-bitwise-and/1963-find-xor-sum-of-all-pairs-bitwise-and.cpp
+++ 1963-find-xor-sum-of-all-pairs-bitwise-and/1963-find-xor-sum-of-all-pairs-bitwise-and.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     int getXORSum(vector<int>& arr1, vector<int>& arr2) {
-        int x1=0,x2=0;
-        for(auto i:arr1)x1^=i;
-        for(auto i:arr2)x2^=i;
-        return x1&x2;
-        // int n=arr1.size(),m=arr2.size();
-        // vector<int>v(32,0),t(32,0);
-        // for(int i=0;i<32;i++){
-        //     int one=0;
-        //     for(int j=0;j<n;j++){
-        //         if((arr1[j]>>i)&1)one++;
-        //     }
-        //     v[i]=one;
-        // }
+        // int x1=0,x2=0;
+        // for(auto i:arr1)x1^=i;
+        // for(auto i:arr2)x2^=i;
+        // return x1&x2;
 
-        // for(int i=0;i<32;i++){
-        //     int one=0;
-        //     for(int j=0;j<m;j++){
-        //         if((arr2[j]>>i)&1)one++;
-        //     }
-        //     t[i]=one;
-        // }
+        int n=arr1.size(),m=arr2.size();
+        vector<int>v(32,0),t(32,0);
+        for(int i=0;i<32;i++){
+            int one=0;
+            for(int j=0;j<n;j++){
+                if((arr1[j]>>i)&1)one++;
+            }
+            v[i]=one;
+        }
 
-        // long long ans=0;
-        // for(int i=0;i<32;i++){
-        //     if((v[i]*1ll*t[i])&1){
-        //         ans+=(1ll<<i);
-        //     }
-        // }
+        for(int i=0;i<32;i++){
+            int one=0;
+            for(int j=0;j<m;j++){
+                if((arr2[j]>>i)&1)one++;
+            }
+            t[i]=one;
+        }
 
-        // return ans;
+        long long ans=0;
+        for(int i=0;i<32;i++){
+            if(v[i]&1 and t[i]&1){
+                ans+=(1ll<<i);
+            }
+        }
+
+        return ans;
     }
 };
