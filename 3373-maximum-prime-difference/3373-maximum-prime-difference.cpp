57bcf2a6cf417b0edac9a3121189f6d8721b8337
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-05-17 13:56:31 +0530

    Time: 82 ms (81.07%), Space: 108.9 MB (50.13%) - LeetHub

--- 3373-maximum-prime-difference/3373-maximum-prime-difference.cpp
+++ 3373-maximum-prime-difference/3373-maximum-prime-difference.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    bool isPrime(int n){
-        if(n<=1)return 0;
-        if(n==2 or n==3)return 1;
-        if(n%2==0 or n%3==0)return 0;
-        for(int i=5;i*i<=n;i+=6){
-            if(n%i==0 or n%(i+2)==0)return 0;
+    void func(vector<bool>&prime){
+        prime[0]=0,prime[1]=0;
+        for(int i=2;i*i<=100;i++){
+            if(prime[i]){
+                for(int j=i*i;j<=100;j+=i){
+                    prime[j]=0;
+                }
+            }
         }
-        return 1;
     }
     int maximumPrimeDifference(vector<int>& nums) {
+        vector<bool>prime(101,1);
+        func(prime);
         int m=1;
         int first,sec;
         for(int i=0;i<nums.size();i++){
-            if(isPrime(nums[i]) and m==1){
+            if(prime[nums[i]] and m==1){
                 first=i;
                 sec=i;
                 m=0;
             }
-            else if(isPrime(nums[i]))sec=i;
+            else if(prime[nums[i]])sec=i;
         }
         return sec-first;
     }
