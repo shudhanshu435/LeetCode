c600735083252cc0a8db6572a8395278b34de3cb
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-05-20 12:48:33 +0530

    Time: 1503 ms (5.04%), Space: 140.1 MB (8.31%) - LeetHub

---
+++ 0959-3sum-with-multiplicity/0959-3sum-with-multiplicity.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mod=1e9+7;
+    int threeSumMulti(vector<int>& arr, int target) {
+        unordered_map<int,int>mp;
+        int n=arr.size();
+        long long ans=0;
+        for(int i=0;i<n-2;i++){
+            for(int m=i+1;m<n;m++)mp[arr[m]]++;
+            for(int j=i+1;j<n-1;j++){
+                mp[arr[j]]--;
+                ans=(ans+mp[target-(arr[i]+arr[j])])%mod;
+                // cout<<ans<<endl;
+            }
+            mp.clear();
+            // cout<<endl;
+        }
+
+        return ans;
+    }
+};
