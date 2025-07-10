a5421a4298c1e829574e4e894f290c4a91d3e91c
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-07-10 17:16:14 +0530

    Time: 13 ms (67.73%), Space: 156.1 MB (24.3%) - LeetHub

--- 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.cpp
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -1,59 +1,37 @@
 class Solution {
 public:
-    int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
-        vector<pair<int,int>>p;
-        int n=startTime.size();
-        for(int i=0;i<n;i++){
-            p.push_back({startTime[i],endTime[i]});
-        }
-        // sort(p.rbegin(),p.rend());
-        vector<int>gap;
-        for(int i=0;i<n;i++){
-            gap.push_back(p[i].second - p[i].first);
-        }
-
-        vector<int>v;
-        v.push_back(p[0].first-0);
-        for(int i=1;i<n;i++){
-            v.push_back(p[i].first-p[i-1].second);
-        }
-        v.push_back(eventTime-p[n-1].second);
-
-        // for(auto i:v)cout<<i<<" ";
-        // cout<<endl;
-        // for(auto i:gap)cout<<i<<" ";
-        // cout<<endl;
-
-        int size=v.size();
-        vector<int>suf(size,0),pre(size,0);
-        pre[0]=v[0];
-        for(int i=1;i<size;i++){
-            pre[i]=max(pre[i-1],v[i]);
-        }
-        suf[size-1]=v[size-1];
-        for(int i=size-2;i>=0;i--){
-            suf[i]=max(suf[i+1],v[i]);
-        }
+    int maxFreeTime(int et, vector<int>& st, vector<int>& en) {
         int ans=0;
-        // for(auto i:pre)cout<<i<<" ";
-        // cout<<endl;
-        // for(auto i:suf)cout<<i<<" ";
-        // cout<<endl;
-        int sum=0;
-        for(int i=0;i<gap.size();i++){
-            sum=v[i]+v[i+1];
-
-            if(i-1>=0 and pre[i-1]>=gap[i]){
-                sum+=gap[i];
-            }
-            else if(i+2<v.size() and suf[i+2]>=gap[i]){
-                sum+=gap[i];
-            }
-            ans=max(ans,sum);
+        int n=st.size();
+        vector<int>gap,spc;
+        gap.push_back(st[0]-0);
+        spc.push_back(en[0]-st[0]);
+        for(int i=0;i<n-1;i++){
+            spc.push_back(en[i+1]-st[i+1]);
+            gap.push_back(st[i+1]-en[i]);
+        }
+        gap.push_back(et-en[n-1]);
+        for(int i=0;i<n;i++){
+            ans=max(ans,gap[i+1]+gap[i]);
+        }
+        // 0 6 1 0
+        // 1 1 1
+        int ss=gap.size();
+        vector<int>pre(ss),suf(ss);
+        pre[0]=gap[0];
+        for(int i=1;i<ss;i++){
+            pre[i]=max(pre[i-1],gap[i]);
+        }
+        suf[ss-1]=gap[ss-1];
+        for(int i=ss-2;i>=0;i--){
+            suf[i]=max(suf[i+1],gap[i]);
         }
 
-
+        for(int i=0;i<n;i++){
+            if((i-1>=0 and spc[i]<=pre[i-1]) or (i+2<=n and spc[i]<=suf[i+2])){
+                ans=max(ans,gap[i]+gap[i+1]+spc[i]);
+            }
+        }
         return ans;
-        
     }
 };
