4270151ed4d5af8b88012d2d65ba312e57d13b3d
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-08-04 14:26:09 +0530

    Time: 169 ms (15.45%), Space: 148.2 MB (30.49%) - LeetHub

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.cpp
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -1,33 +1,67 @@
 class Solution {
 public:
-    int f(int l , int r,vector<int>& prefix){
-        if(l>r) return 0 ;
-        return prefix[r]-((l>0)? prefix[l-1] : 0);
-    }
-    int maxTotalFruits(vector<vector<int>>& fruits, int start, int k) {
-        int n = fruits.size();
-        int maxi = fruits.back()[0];
-        int ans = 0;
-        vector<int>prefix(maxi+1);
-        for(auto it : fruits){
-            prefix[it[0]]=it[1];
-        }
-        for(int i = 1 ; i <maxi+1 ; i++){
-            prefix[i]+=prefix[i-1];
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        int n=fruits.size();
+        vector<int>pref(n);
+        pref[0]=fruits[0][1];
+        for(int i=1;i<n;i++)pref[i]=pref[i-1]+fruits[i][1]; //prefix sum of fruits
+        int maxi=0;
+        int p=startPos;
+        int left;
+        int right;
+        int ind1;
+        int ind2;
+        vector<int>pos;
+        for(auto i:fruits)pos.push_back(i[0]);
+
+        // go left only
+        int leff=startPos-k;
+        if(startPos>=pos[0]){
+            leff=max(leff,0);
+            auto it1=lower_bound(pos.begin(),pos.end(),leff);
+            int i1=it1-pos.begin();
+            auto up1=upper_bound(pos.begin(),pos.end(),startPos);
+            int i2=up1-pos.begin();
+            if(i1>0)maxi=max(maxi,pref[i2-1]-pref[i1-1]);
+            else maxi=max(maxi,pref[i2-1]);
         }
 
-        int left = max(start-k,0);
-        for(int right = left ; right <= min(start + k, maxi); right++){
-           while (left <= right &&
-                  !( (2 * (start - left) + (right - start) <= k) ||
-                     (2 * (right - start) + (start - left) <= k) )) {
-                left++;
-            }
-            if(left<=right){
-                ans=max(ans,f(left,right,prefix));
-            }
-        }
 
-        return ans ;
+        while(p>=0){
+            left=k-2*(startPos-p);
+            // if(left<0)break;
+            if(startPos-p>k)break;
+            right=startPos+left;
+            auto it=lower_bound(pos.begin(),pos.end(),p);
+            ind1=it-pos.begin();
+            auto rt=upper_bound(pos.begin(),pos.end(),right);
+            ind2=rt-pos.begin()-1;
+            if(ind1<=ind2 and ind2!=-1){
+                // cout<<p<<" "<<ind1<<endl;
+                // cout<<right<<" "<<ind2<<endl;
+
+                if(ind1>0) maxi=max(maxi,pref[ind2]-pref[ind1-1]);
+                else maxi=max(maxi,pref[ind2]);
+                // cout<<maxi<<endl<<endl;
+            }
+            p--;
+        }
+        // cout<<maxi<<endl;
+        p=startPos;
+        while(p<=fruits[n-1][0]){
+            left=k-2*(p-startPos);
+            if(left>k)break;
+            int goleft=startPos-left;
+            auto iit=lower_bound(pos.begin(),pos.end(),goleft);
+            ind1=iit-pos.begin();
+            auto rrt=upper_bound(pos.begin(),pos.end(),p);
+            ind2=rrt-pos.begin()-1;
+            if(ind1<=ind2 and ind2!=-1){
+                if(ind1>0) maxi=max(maxi,pref[ind2]-pref[ind1-1]);
+                else maxi=max(maxi,pref[ind2]);
+            }
+            p++;
+        }
+        return maxi;
     }
 };
