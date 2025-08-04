1306a172e547a48979e66d086bfca13b7b1d89fe
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-08-05 00:18:32 +0530

    Time: 8 ms (84.25%), Space: 56.8 MB (35.32%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.cpp
+++ 1972-rotating-the-box/1972-rotating-the-box.cpp
@@ -1,56 +1,40 @@
 class Solution {
 public:
-    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
-        int m=box.size();
-        int n=box[0].size();
+    vector<vector<char>> rotateMatrix(vector<vector<char>>& matrix) {
+        int m = matrix.size();        // rows
+        int n = matrix[0].size();     // cols
 
-        vector<vector<char>>vec(n, vector<char>(m));
-        //transpose
+        vector<vector<char>> rotated(n, vector<char>(m));
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                rotated[j][m - 1 - i] = matrix[i][j];
+            }
+        }
+
+        return rotated;
+    }
+
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+        int n=boxGrid.size();
+        int m=boxGrid[0].size();
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                vec[i][j]=box[j][i];
+            int x=m-1,y=m-1;
+            while(x>=0){
+                if(boxGrid[i][x]=='#'){
+                    boxGrid[i][y]='#';
+                    if(y!=x)boxGrid[i][x]='.';
+                    y--;
+                }
+                else if(boxGrid[i][x]=='*')y=x-1;
+                x--;
             }
         }
-
-        //90 degree rotate
-        for(vector<char>& row: vec){
-            reverse(row.begin(),row.end());
-        }
-
-        for(int j=0;j<m;j++){
-            int space=n-1;
-            for(int i=n-1;i>=0;i--){
-                if(vec[i][j]=='*'){
-                    space=i-1;
-                    continue;
-                }
-                if(vec[i][j]=='#'){
-                    vec[i][j]='.';
-                    vec[space][j]='#';
-                    space--;
-                }
-            }
-        }
-
-        // for(int j=0;j<m;j++){
-        //     for(int i=n-1;i>=0;i--){
-        //         if(vec[i][j]=='.'){
-        //             int row=-1;
-        //             for(int k=i-1;k>=0;k--){
-        //                 if(vec[k][j]=='*'){
-        //                     break;
-        //                 } else if(vec[k][j]=='#'){
-        //                     row=k;
-        //                     break;
-        //                 }
-        //             }
-        //             if(row!=-1){
-        //                 vec[i][j]='#';
-        //                 vec[row][j]='.';
-        //             }
-        //         }
-        //     }
+        // for(auto i:boxGrid){
+        //     for(auto v:i)cout<<v<<" ";
+        //     cout<<endl;
         // }
-        return vec;
+        vector<vector<char>>v=rotateMatrix(boxGrid);
+        return v;
     }
 };
