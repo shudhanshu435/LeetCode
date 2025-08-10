2d427c3c0e42873f737991681c494ce330637fa1
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-08-10 18:02:37 +0530

    Time: 0 ms (100%), Space: 8.9 MB (62.83%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
-        unordered_map<int,int>mp;
-        while(n){
-            mp[n%10]++;
-            n/=10;
-        }
-        long long p=0;
+        string s=to_string(n);
+        sort(s.begin(),s.end());
+
         for(int i=0;i<32;i++){
-            unordered_map<int,int>mp2;
             int m=(1<<i);
-            while(m){
-                mp2[m%10]++;
-                m/=10;
-            }
-            int cnt=0;
-            if(mp==mp2)return 1;
+            string ss=to_string(m);
+            sort(ss.begin(),ss.end());
+            if(s==ss)return 1;
         }
         return 0;
     }
