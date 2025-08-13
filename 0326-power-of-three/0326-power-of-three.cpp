50e5b617240589834cac6a1454b6d483fa3effc6
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-08-13 11:12:44 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.26%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public: 
-    bool recur(int n){
-        if(n==1)return 1;
-        if(n%3!=0 or n==0)return 0;
-        return recur(n/3);
-    }
+    
     bool isPowerOfThree(int n) {
-        return recur(n);
+        return n > 0 && 1162261467 % n == 0;
     }
+
 };
