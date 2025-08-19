2b8b56c8fc28e97a00c70e734eb00d294c9d126e
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-08-19 15:32:32 +0530

    Time: 4 ms (27.82%), Space: 111.5 MB (11.1%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -5,7 +5,7 @@ public:
         int z=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==0)z++;
-            else{
+            else if(z!=0){
                 ans+=z*1ll*(z+1)/2;
                 z=0;
             }
