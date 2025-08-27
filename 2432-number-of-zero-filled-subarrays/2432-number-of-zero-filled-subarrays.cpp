17a2f0dc4ccf044e659b13a153cf54c00eff617a
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-08-27 23:08:35 +0530

    Time: 3 ms (38.35%), Space: 111.4 MB (42.59%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -3,14 +3,25 @@ public:
     long long zeroFilledSubarray(vector<int>& nums) {
         long long ans=0;
         int z=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0)z++;
-            else if(z!=0){
-                ans+=z*1ll*(z+1)/2;
-                z=0;
+        if(nums[0]==0)z++;
+        ans+=z;
+        for(int i=1;i<nums.size();i++){
+            // if(nums[i]==0)z++;
+            // else if(z!=0){
+            //     ans+=z*1ll*(z+1)/2;
+            //     z=0;
+            // }
+            if(nums[i]==0 and nums[i-1]==0){
+                z=z+1;
             }
+            else if(nums[i]==0){
+                z=1;
+            }
+            else z=0;
+            ans+=z;
+
+            // cout<<i<< " "<<ans<<endl;
         }
-        ans+=z*1ll*(z+1)/2;
         return ans;
     }
 };
