47e59a3f72f65b789ea28b88afad5e859983b8af
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-09-04 01:12:02 +0530

    Time: 0 ms (100%), Space: 8.5 MB (12.66%) - LeetHub

--- 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        int o1=0,e1=0; 
-        int o2=0,e2=0;
-        e1=n/2;
-        if(n&1)o1=(n+1)/2;
-        else o1=n/2;
-        e2=m/2;
-        if(m&1)o2=(m+1)/2;
-        else o2=m/2;
-        return (o1*1ll*e2)+(e1*1ll*o2);
+        return ((n+1)/2 )*1ll* (m/2) + (n/2)*1ll*((m+1)/2);
     }
 };
