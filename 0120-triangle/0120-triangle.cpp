85832b1ae89d311fe174d6761a7f473274e28704
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-09-25 12:08:27 +0530

    Time: 0 ms (100%), Space: 12.5 MB (97.14%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& tt) {
         int n=tt.size();
-        vector<vector<int>>dp(n,vector<int>(n));
-        dp[0][0]=tt[0][0];
         for(int i=1;i<n;i++){
             for(int j=0;j<=i;j++){
-                // cout<<tt[i][j]<<" ";
                 if(i-1>=j){
-                    if(i-1>=0 and j-1>=0)dp[i][j]=tt[i][j] + min(dp[i-1][j],dp[i-1][j-1]);
-                    else dp[i][j]=dp[i-1][j]+tt[i][j];
+                    if(i-1>=0 and j-1>=0)tt[i][j]+=min(tt[i-1][j],tt[i-1][j-1]);
+                    else tt[i][j]+=tt[i-1][j];
                 }
-                else dp[i][j]=tt[i][j] + dp[i-1][j-1];
+                else tt[i][j]+=tt[i-1][j-1];
             }
         }
         int mini=INT_MAX;
-        for(auto &i:dp[n-1])mini=min(mini,i);
+        for(auto &i:tt[n-1])mini=min(mini,i);
         return mini;
     }
 };
