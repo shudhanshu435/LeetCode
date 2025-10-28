4996e6791dc543bcbd871a826c8ffa57afb5e73e
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-10-28 11:34:00 +0530

    Time: 0 ms (100%), Space: 21.9 MB (94.04%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>vec(n+1,0),tec(n+1,0);
-        vec[0]=0;
-        for(int i=1;i<=n;i++){
-            vec[i]=nums[i-1]+vec[i-1];
-        }
-        tec[n]=0;
-        for(int i=n-1;i>=0;i--){
-            tec[i]=nums[i]+tec[i+1];
-        }
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        int curr=0;
         int ans=0;
-
-        for(auto i:vec){
-            cout<<i<<" ";
-        }
-        cout<<endl;
-        for(auto i:tec){
-            cout<<i<<" ";
-        }
-        cout<<endl;
-        
-
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.size();i++){
+            curr+=nums[i];
             if(nums[i]==0){
-                if(vec[i]==tec[i+1])ans+=2;
-                else if(abs(vec[i]-tec[i+1])==1)ans+=1;
+                if(curr==sum-curr)ans+=2;
+                else if(abs(curr-(sum-curr))==1)ans+=1;
             }
         }
         return ans;
