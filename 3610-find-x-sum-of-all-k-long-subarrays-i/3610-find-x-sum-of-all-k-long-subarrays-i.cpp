4485e18818a9bc14d777530443716ee8f8caffb0
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-11-06 00:58:23 +0530

    Time: 7 ms (90.03%), Space: 31.7 MB (97.43%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -3,24 +3,40 @@ public:
     vector<int> findXSum(vector<int>& nums, int k, int x) {
         int n = nums.size();
         vector<int> ans;
-        for (int i = 0; i <= n - k; ++i) {
-            unordered_map<int, int> cnt;
-            for (int j = i; j < i + k; ++j) {
-                ++cnt[nums[j]];
-            }
+        ans.reserve(max(0, n - k + 1));
 
-            vector<pair<int, int>> freq;
-            for (const auto& [key, value] : cnt) {
-                freq.emplace_back(value, key);
-            }
-            sort(freq.begin(), freq.end(), greater<pair<int, int>>());
+        unordered_map<int,int> freq;
 
-            int xsum = 0;
-            for (int j = 0; j < x && j < freq.size(); ++j) {
-                xsum += freq[j].first * freq[j].second;
+        for (int i = 0; i < k; ++i) freq[nums[i]]++;
+
+        auto compute_x_sum = [&](unordered_map<int,int>& f)->int64_t {
+            vector<pair<int,int>> items;
+            items.reserve(f.size());
+            for (auto &p : f) items.push_back({p.first, p.second});
+            sort(items.begin(), items.end(), [](const auto& a, const auto& b){
+                if (a.second != b.second) return a.second > b.second;
+                return a.first > b.first;
+            });
+            long long sum = 0;
+            int take = min<int>(x, (int)items.size());
+            for (int i = 0; i < take; ++i) {
+                sum += 1LL * items[i].first * items[i].second;
             }
-            ans.push_back(xsum);
+            return sum;
+        };
+
+        ans.push_back((int)compute_x_sum(freq));
+
+        for (int i = k; i < n; ++i) {
+            int add = nums[i];
+            int rem = nums[i - k];
+
+            freq[add]++;
+            if (--freq[rem] == 0) freq.erase(rem);
+
+            ans.push_back((int)compute_x_sum(freq));
         }
+
         return ans;
     }
 };
