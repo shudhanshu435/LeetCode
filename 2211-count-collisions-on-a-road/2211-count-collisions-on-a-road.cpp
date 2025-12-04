64391b3c62adc534a370d31ded5a033e040e72ef
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2025-12-04 15:42:16 +0530

    Time: 0 ms (100%), Space: 19.1 MB (96.23%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    int countCollisions(string dir) {
-        int cnt=0;
-        int n=dir.size();
+    int countCollisions(string s) {
+        int n=s.size();
         int i=0,j=n-1;
-        while(i<n and dir[i]=='L')i++;
-        while(j>=0 and dir[j]=='R')j--;
-        if(i>j)return 0;
-        for(;i<j;i++){
-            if(dir[i]=='S' and (dir[i+1]=='S' or dir[i+1]=='R'))continue;
-            if(dir[i]=='S')cnt++;
-            else if(dir[i]=='R' and dir[i+1]=='L')cnt+=2;
-            else if(dir[i]=='R' and (dir[i+1]=='S' or dir[i+1]=='R'))cnt++;
-            else if(dir[i]=='L' and (dir[i+1]=='S' or dir[i]=='R'))continue;
-            if(dir[i]=='L' and dir[i+1]=='L')cnt++;
-        }
+        while(i<n && s[i]=='L')i++;  
+        while(j>=0 && s[j]=='R')j--;
+        int cnt=0;
+        for(int k=i;k<=j;k++)
+            if(s[k]!='S')cnt++;
         return cnt;
     }
 };
