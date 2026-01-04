aa94456db23e78f1f06fe0f388b38e2abdfba286
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2026-01-04 23:19:41 +0530

    Time: 35 ms (33.22%), Space: 11.2 MB (33.22%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    int numOfWays(int n) {
-        int mod=1000000007;
-        if(n==1)return 12;
-        long long a=6,b=6;
-        for(int i=2;i<=n;i++){
-            long long na=(a*2+b*2)%mod;
-            long long nb=(a*2+b*3)%mod;
-            a=na;
-            b=nb;
+    long long  dp[5001][4][4][4];
+    int mod=1e9+7;
+    int rec(int x, int a, int b, int c){
+        if(x==0)return 1;
+
+        if(dp[x][a][b][c]!=-1)return dp[x][a][b][c];
+        long long ans=0;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                for(int k=0;k<3;k++){
+                    if(i==j or j==k)continue;
+                    if(i==a or j==b or k==c)continue;
+                    ans=(ans+(rec(x-1,i,j,k)))%mod;
+                }
+            }
         }
-        return (a+b)%mod;
+        return dp[x][a][b][c]=ans;
+    }
+    int numOfWays(int n) {
+        memset(dp,-1,sizeof(dp));
+        return rec(n,3,3,3);
     }
 };
