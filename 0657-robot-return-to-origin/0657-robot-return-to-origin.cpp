ec6276e2a1adea8d16a3c1ba252a7d1c03e0073d
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2026-04-05 12:00:06 +0530

    Time: 3 ms (46.4%), Space: 10.4 MB (47.26%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x=0,y=0;
+        for(auto ch:moves){
+            if(ch=='U')y++;
+            else if(ch=='D')y--;
+            else if(ch=='L')x--;
+            else x++;
+        }
+        return x==0 and y==0;
+    }
+};
