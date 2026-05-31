2aeaa16a94eb9578f21c7fe153f789d5888df0d5
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2026-05-31 07:47:00 +0530

    Time: 39 ms (24.82%), Space: 106.8 MB (16.2%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    bool asteroidsDestroyed(int mass, vector<int>& a) {
-        sort(a.begin(),a.end());
-        int n=a.size();
-        long long tot=mass;
-        for(int i=0;i<n;i++){
-            if(tot<a[i])return 0;
-            tot+=a[i];
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        long long m=mass;
+        sort(asteroids.begin(),asteroids.end());
+        for(auto i:asteroids){
+            if(m<i)return 0;
+            m+=i;
         }
         return 1;
     }
