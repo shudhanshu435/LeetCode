634570132ac1e63d0e86d1ae1131986f4658674a
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2026-06-09 22:35:59 +0530

    Time: 3 ms (31.53%), Space: 18.7 MB (5.66%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -11,24 +11,29 @@
  */
 class Solution {
 public:
-    void rec(vector<string>&ans, TreeNode*root,vector<int>v){
-        v.push_back(root->val);
+    void dfs(vector<vector<int>>&v, TreeNode* root, vector<int>vv){
+        vv.push_back(root->val);
         if(!root->left and !root->right){
-            string a="";
-            for(int i=0;i<v.size();i++){
-                a+=to_string(v[i]);
-                if(i<v.size()-1)a+="->";
-            }
-            ans.push_back(a);
-            v.clear();
+            v.push_back(vv);
         }
-        if(root->left)rec(ans,root->left,v);
-        if(root->right)rec(ans,root->right,v);
+        if(root->left)dfs(v,root->left,vv);
+        if(root->right)dfs(v,root->right,vv);
     }
     vector<string> binaryTreePaths(TreeNode* root) {
-        vector<int>v;
-        vector<string>ans;
-        rec(ans,root,v);
-        return ans;
+        if(!root)return {};
+        vector<vector<int>>v;
+        vector<int>vv;
+        dfs(v,root,vv);
+        vector<string>vec;
+        for(auto i:v){
+            string s="";
+            for(auto ii:i){
+                s+=to_string(ii)+"->";
+            }
+            s.pop_back();
+            s.pop_back();
+            vec.push_back(s);
+        }
+        return vec;
     }
 };
