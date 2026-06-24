8dcbfdd6a213b437e1d9fbc8d8e58abdaf0c8889
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2026-06-24 10:40:17 +0530

    Time: 0 ms (100%), Space: 22.5 MB (55.05%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -13,14 +13,16 @@ class Solution {
 public:
     pair<int,int> dfs(TreeNode* root){
         if(!root)return {0,0};
-        auto [a,b]=dfs(root->left);
-        auto [c,d]=dfs(root->right);
-        int t=b+root->val+d;
-        int nt=max(a,b)+max(c,d);
+        pair<int,int>a=dfs(root->left);
+        pair<int,int>b=dfs(root->right);
+        int t=root->val+a.second+b.second;
+        int nt=max(a.first,a.second)+max(b.first,b.second);
+
         return {t,nt};
+
     }
     int rob(TreeNode* root) {
-        auto [t,nt]=dfs(root);
-        return max(t,nt);
+        pair<int,int>p=dfs(root);
+        return max(p.first,p.second);
     }
 };
