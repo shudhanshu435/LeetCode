444f302d27d0d5a36dccb91b236039c38841a74a
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2026-06-30 16:29:00 +0530

    Time: 30 ms (27.7%), Space: 73.2 MB (24.21%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        unordered_map<int,int>mp;
-        map<pair<int,int>,bool>mp2;
+        vector<int>a(n,0),b(n,0);
         for(auto i:trust){
-            mp[i[0]-1]++;
-            mp2[{i[0],i[1]}]=1;
+            a[i[0]-1]++;  //outdegree
+            b[i[1]-1]++;  //indegree
         }
-        int c=-1;
-        for(int i=0;i<n;i++)if(mp[i]==0)c=i+1;
-        if(c==-1)return -1;
-        for(int i=1;i<n;i++){
-            if(i!=c){
-                if(!mp2[{i,c}])return -1;
-            }
+        for(int i=0;i<n;i++){
+            if(a[i]==0 and b[i]==n-1)return i+1;
         }
-        return c;
+        return -1;
     }
 };
