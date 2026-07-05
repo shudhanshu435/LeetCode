2899801ccbad36086c789724693ae6ffb864d2f5
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2026-07-06 00:25:59 +0530

    Time: 11 ms (48.08%), Space: 11.7 MB (58.19%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
@@ -1,55 +1,59 @@
 class Solution {
 public:
-int mod=1000000007;
-    int totalpaths(vector<string>& grid, int K) {
-        int n=grid.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(n,vector<int>(K+1,0)));
-        dp[0][0][0]=1;
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 and j==0)continue;
-                if(grid[i][j]=='X')continue;
-                int v=grid[i][j]-'0';
-                for(int k=v;k<=K;k++){
-                    if(i>0)dp[i][j][k]=(dp[i][j][k]+dp[i-1][j][k-v])%mod;
-                    if(j>0)dp[i][j][k]=(dp[i][j][k]+dp[i][j-1][k-v])%mod;
-                    if(i>0 and j>0)dp[i][j][k]=(dp[i][j][k]+dp[i-1][j-1][k-v])%mod;
-                }
-            }
-        }
-        return (dp[n-1][n-1][K])%mod;
-    }
+    const int MOD=1e9+7;
     vector<int> pathsWithMaxScore(vector<string>& board) {
         int n=board.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        board[0][0]='0';
-        board[n-1][n-1]='0';
-        dp[0][0]=0;
-        for(int i=1;i<n;i++){
-            if(board[0][i]=='X' or dp[0][i-1]==-1)dp[0][i]=-1;
-            else dp[0][i]=(board[0][i]-'0')+dp[0][i-1];
+        vector<vector<int>>vec(n,vector<int>(n,0));
+        vector<vector<int>>way(n,vector<int>(n,0));
+        way[0][0]=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]>='0' and board[i][j]<='9'){
+                    vec[i][j]=board[i][j]-'0';
+                }
+                else vec[i][j]=-1;
+            }
         }
-
-        for(int i=1;i<n;i++){
-            if(board[i][0]=='X' or dp[i-1][0]==-1)dp[i][0]=-1;
-            else dp[i][0]=(board[i][0]-'0')+dp[i-1][0];;
+        vec[0][0]=0;
+        vec[n-1][n-1]=0;
+        bool b=0;
+        for(int i=0;i<n;i++){
+            if(vec[0][i]==-1)b=1;
+            else if(b)vec[0][i]=-1;
+            else if(i)vec[0][i]+=vec[0][i-1];
+            if(vec[0][i]!=-1 and i)way[0][i]=way[0][i-1];
+        }
+        b=0;
+        for(int i=0;i<n;i++){
+            if(vec[i][0]==-1)b=1;
+            else if(b)vec[i][0]=-1;
+            else if(i)vec[i][0]+=vec[i-1][0];
+            if(vec[i][0]!=-1 and i)way[i][0]=way[i-1][0];
         }
 
         for(int i=1;i<n;i++){
             for(int j=1;j<n;j++){
-                if(board[i][j]=='X')dp[i][j]=-1;
-                else{
-                    int best=max({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]});
-                    if(best==-1)dp[i][j]=-1;
-                    else dp[i][j]=(board[i][j]-'0')+best;
+                if(vec[i][j]==-1)continue;
+                int best=max({vec[i-1][j],vec[i-1][j-1],vec[i][j-1]});
+                if(best==-1){
+                    vec[i][j]=-1;continue;
                 }
+                vec[i][j]+=best;
+                if(vec[i-1][j]==best)way[i][j]=(way[i][j]+way[i-1][j])%MOD;
+                if(vec[i-1][j-1]==best)way[i][j]=(way[i][j]+way[i-1][j-1])%MOD;
+                if(vec[i][j-1]==best)way[i][j]=(way[i][j]+way[i][j-1])%MOD;
             }
         }
-        // cout<<dp[n-1][n-1]<<endl;
-        if(dp[n-1][n-1]==-1)return {0,0};
-        return {dp[n-1][n-1], totalpaths(board,dp[n-1][n-1])};
 
-        return {};
+        if(vec[n-1][n-1]==-1)return {0,0};
+        return {vec[n-1][n-1],way[n-1][n-1]};
+
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<vec[i][j]<<' ';
+        //     }
+        //     cout<<endl;
+        // }
+        
     }
 };
