b27250225f26595ef9a2d285c5be54541e667356
Shudhanshu Kumar <sudhanshukumar1242002@gmail.com>
2026-08-02 23:09:14 +0530

    Time: 6 ms (35.88%), Space: 11.5 MB (37.34%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int dp[501][501];
-    int rec(vector<int>&piles, int i, int j){
+    int memo[501][501];
+    int rec(vector<int>& piles, int i, int j){
         if(i>j)return 0;
-        //take
-        if(dp[i][j]!=-1)return dp[i][j];
-        return dp[i][j]=min(piles[i]+rec(piles,i+2,j),piles[j]+rec(piles,i,j-2));
+        if(memo[i][j]!=-1)return memo[i][j];
+        int lt=piles[i]+min(rec(piles,i+2,j),rec(piles,i+1,j-1));
+        int rt=piles[j]+min(rec(piles,i+1,j-1),rec(piles,i,j-2));
 
+        return memo[i][j]=max(lt,rt);
     }
     bool stoneGame(vector<int>& piles) {
-        int n=piles.size()-1;
-        memset(dp,-1,sizeof(dp));
-        return rec(piles,0,n);
+        memset(memo,-1,sizeof(memo));
+        int alice=rec(piles,0,piles.size()-1);
+        int sum=accumulate(piles.begin(),piles.end(),0);
+        return alice>=sum-alice;
     }
 };
